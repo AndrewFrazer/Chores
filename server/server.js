7dea048da71a4ce6b910c7fc643c71042d0c3127
Andrew Frazer <andrewsfrazer@gmail.com>
2019-04-27 18:25:01 +0100

    Constructing a schema from typedefs and resolvers. Requires graphql-tools Removed unused code from script.js

--- server/server.js
+++ server/server.js
@@ -1,33 +1,43 @@
 /* eslint-disable no-console */
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
+const { makeExecutableSchema } = require('graphql-tools');
 const cors = require('cors');
 const uuidv4 = require('uuid/v4');
 
-// Construct a schema, using GraphQL schema language
-const schema = buildSchema(`
+// Setup type defs, using GraphQL schema language
+const typeDefs = `
   input ChoreInput {
     chore: String
     points: Int
+    time: Float
   }
 
   type User {
-    id: ID!
+    id: ID
     name: String
+    points: Int
+  }
+
+  type Chore {
+    userId: ID
+    chore: String
+    points: Int
+    time: Float
   }
 
   type Query {
-    getUsers: [User]
-    getPoints(id: ID): Int
-    getChores(id: ID): [String]
+    user(id: ID!): User
+    users: [User]
+    chores(userId: ID): [Chore]
   }
 
   type Mutation {
-    setChore(id: ID, input: ChoreInput): Int
+    setChore(userId: ID, input: ChoreInput): Int
   }
-`);
+`;
 
+// Classes could be moved to another file
 class User {
   constructor(id, name) {
     this.id = id;
@@ -36,56 +46,95 @@ class User {
 }
 
 class Chore {
-  constructor(id, {chore, points}) {
+  constructor(id, {chore, points, time}) {
     this.userId = id;
     this.chore = chore;
     this.points = points;
+    this.time = time;
   }
 }
 
-var userDb = {};
+// Setup fake DBs
+let userDb = {};
 userDb[uuidv4()] = "Andy";
 userDb[uuidv4()] = "Torie";
-var choreDb = [];
+let choreDb = [];
 
-// The root provides a resolver function for each API endpoint
-const root = {
-  getUsers: () => {
-    var userArray = [];
-    for (const userId in userDb) {
-      var user = new User(userId, userDb[userId])
-      userArray.push(user);
-    }
-    return userArray;
-  },
-  getPoints: function ({id}) {
-    let chores = choreDb.filter(x => x.userId == id);
-    let totalPoints = 0;
-    chores.forEach(chore => {
-      totalPoints += chore.points
-    });
-    return totalPoints;
-  },
-  getChores: function ({id}) {
-    let chores = choreDb.filter(x => x.userId == id);
-    let choreNames = [];
-    chores.forEach(chore => {
-      choreNames.push(chore.chore);
-    });
-    return choreNames;
-  },
-  setChore: function ({id, input}) {
-    choreDb.push(new Chore(id, input));
-    return input.points;
-  },
-};
+// Define functions for the resolver
+const getUser = (id) => {
+  console.log('getUser: ' + id);
+  return new User(id, userDb[id]);
+}
 
+const getUserPoints = (id) => {
+  console.log('getUserPoints: ' + id);
+  let chores = choreDb.filter(x => x.userId == id);
+  let totalPoints = 0;
+  chores.forEach(chore => {
+    totalPoints += chore.points;
+  });
+  return totalPoints;
+}
+
+const getUsers = () => {
+  console.log('getUsers');
+  let userArray = [];
+  for (const userId in userDb) {
+    let user = new User(userId, userDb[userId]);
+    userArray.push(user);
+  }
+  return userArray;
+}
+
+const getChores = (id) => {
+  console.log('getChores: ' + id);
+  let chores = [];
+  if (id) {
+    chores = choreDb.filter(x => x.userId == id);
+  } else {
+    chores = choreDb;
+  }
+  return chores;
+}
+
+const setChore = (userId, input) => {
+  console.log('setChore: ' + userId);
+  console.log('input: ' + input.chore + ' ' + input.time);
+  choreDb.push(new Chore(userId, input));
+  return input.points;
+}
+
+// Setup the resolver
+const resolvers = {
+  Query: {
+    user: async (root, {id}) => await getUser(id),
+    users: async (root, {id}) => await getUsers(),
+    chores: async (root, {id}) => await getChores(id),
+  },
+  Mutation: {
+    setChore: async (root, {userId, input}) => await setChore(userId, input),
+  },
+  User: {
+    points: async ({id}) => await getUserPoints(id),
+  },
+}
+
+// Create schema using type defs and resolvers
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+// Setup app
 const app = express();
 app.use(cors())
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  rootValue: root,
   graphiql: true,
 }));
 app.listen(4000);
+// Some server info
 console.log('Running a GraphQL API server at localhost:4000/graphql');
+for (const userId in userDb) {
+  console.log(userDb[userId] + ':' + userId);
+}
